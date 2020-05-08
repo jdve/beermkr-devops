f8c781c54fc267eb16c3532e59e2fcc3d603cf6c
Jonathan Van Eenwyk <jonathan@vihanti.com>
2020-05-08 07:15:57 -0700

    Add task_role_arn for tasks to access cloudwatch

---
+++ terraform/production/api/iam_task_role.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "task_role" {
+  name = "${local.environment}-${local.generation}-${local.app}-ecs-task-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "task_policy" {
+  name_prefix = "${local.environment}-${local.generation}-${local.app}-task-policy"
+  description = ""
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Sid": "statement0",
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": [
+          "${aws_cloudwatch_log_group.log_group_machines.arn}"
+        ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "task_role_attach_1" {
+    role       = aws_iam_role.task_role.name
+    policy_arn = aws_iam_policy.task_policy.arn
+}
